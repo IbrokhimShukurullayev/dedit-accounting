a48ba26caa0ded7de0c58b07211b8fe3a42f71fa
ibrohim <https://github.com/IbrokhimShukurullayev>
2025-04-08 22:56:00 +0500

    ibrokhimm

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -156,7 +156,7 @@ const Header = ({ activeSection }: HeaderProps) => {
               </a>
             ))}
             <a
-              href="tel:+11234567890"
+              href="tel:++998 33 133 93 33"
               className="flex items-center py-2 font-medium text-primary"
               onClick={() => setIsMobileMenuOpen(false)}
             >
@@ -174,7 +174,7 @@ const Header = ({ activeSection }: HeaderProps) => {
                   d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                 />
               </svg>
-              +1 (123) 456-7890
+              +998 33 133 93 33
             </a>
           </nav>
         </div>
